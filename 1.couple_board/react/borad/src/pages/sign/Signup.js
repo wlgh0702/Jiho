7789d82da879857b2a5df14a12c845d0a2120358
DESKTOP-TFLQRBN\jihopark <wlghsms987@gmail.com>
2023-10-30 18:40:26 +0900

    10/30 회원가입 폼 만드는 중

--- 1.couple_board/react/borad/src/pages/sign/Signup.js
+++ 1.couple_board/react/borad/src/pages/sign/Signup.js
@@ -9,33 +9,31 @@ function Signup() {
                     <div className={s.logo}>
                         <img src={logo} style={{height : 80}}/>
                     </div>
+                    <br/>
                     <div className={s.info}>
-                        <pre style={{margin : 0}}><b>아이디</b></pre>
-                        <input type="text"/>
+                        <div className={s.infoHead}>아이디</div>
+                        <div className={s.infoBody}>
+                            <input type="text"/>
+                            <div className={s.duplicate}>중복확인</div>
+                        </div>
                     </div>
                     <div className={s.info}>
-                        <pre style={{margin : 0}}><b>비밀번호</b></pre>
-                        <input type="password"/>
+                        <div className={s.infoHead}>비밀번호</div>
+                        <div className={`${s.infoBody} ${s.pwd}`}>
+                            <input type="password"/>
+                        </div>
                     </div>
                     <div className={s.info}>
-                        <pre style={{margin : 0}}><b>비밀번호 확인</b></pre>
-                        <input type="password"/>
+                        <div className={s.infoHead}>비밀번호 확인</div>
+                        <div className={`${s.infoBody} ${s.pwd}`}>
+                            <input type="password"/>
+                        </div>
                     </div>
                     <div className={s.info}>
-                        <pre style={{margin : 0}}><b>이름</b></pre>
-                        <input type="text"/>
-                    </div>
-                    <div className={s.info}>
-                        <pre style={{margin : 0}}><b>이메일</b></pre>
-                        <input type="text"/>
-                    </div>
-                    <div className={s.info}>
-                        <pre style={{margin : 0}}><b>생년월일</b></pre>
-                        <input type="text"/>
-                    </div>
-                    <div className={s.info}>
-                        <pre style={{margin : 0}}><b>성별</b></pre>
-                        <input type="text"/>
+                        <div className={s.infoHead}>이름</div>
+                        <div className={`${s.infoBody} ${s.pwd}`}>
+                            <input type="text"/>
+                        </div>
                     </div>
                 </div>
             </div>
